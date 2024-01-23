ccbaf36baa9659222ff8d2161857dd1b6702883b
Diwansh Gupta <130465365+DiwanshGupta@users.noreply.github.com>
2024-01-23 20:49:32 +0530

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const connectDB = require("./utility/db");
 require("dotenv").config();
 const Cors = require("cors");
 const corseoptions = {
-  origin: "https://dmern.vercel.app/",
+  origin: "*",
   methods: "GET,PUT,DELETE,POST,PATCH,HEAD,OPTIONS",
   credentials: true,
 };
