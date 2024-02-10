866f695f6d2e472518f95dc5159b57823eab2d6c
Diwansh Gupta <130465365+DiwanshGupta@users.noreply.github.com>
2024-02-10 17:15:11 +0530

    Update db.js

--- utility/db.js
+++ utility/db.js
@@ -1,21 +1,3 @@
-// const mongoose = require("mongoose");
-
-// const URL =
-//   "mongodb+srv://dewanshgupta120:kirito123@devmern.pauihib.mongodb.net/";
-
-// const connectdb = async () => {
-//   try {
-//     await mongoose.connect(URL);
-
-//     console.log(`Connected successfully to mongoose`);
-//   } catch (error) {
-//     console.error("Error connecting to MongoDB:", error.message);
-//     process.exit(1); // Exit the application with an error code
-//   }
-// };
-
-// module.exports = connectdb;
-
 const mongoose = require("mongoose");
 const URI = process.env.MOngourl;
 const connectDb = async (req, res) => {
